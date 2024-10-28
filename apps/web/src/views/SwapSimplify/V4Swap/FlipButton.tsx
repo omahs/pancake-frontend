597a12ffe53020a83ec86f639525050aa266ef22
Chef Penguin <penguin@pancakeswap.com>
2024-10-28 18:54:53 +0530

    chore: swap switch arrow animation

--- apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
+++ apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
@@ -2,7 +2,7 @@ import { AutoColumn, Button } from '@pancakeswap/uikit'
 
 import { useTranslation } from '@pancakeswap/localization'
 import replaceBrowserHistory from '@pancakeswap/utils/replaceBrowserHistory'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useMemo, useRef } from 'react'
 
 import { AutoRow } from 'components/Layout/Row'
 import { Field } from 'state/swap/actions'
@@ -10,8 +10,12 @@ import { useSwapState } from 'state/swap/hooks'
 import { useSwapActionHandlers } from 'state/swap/useSwapActionHandlers'
 import { styled } from 'styled-components'
 
-import { SwapUIV2 } from '@pancakeswap/widgets-internal'
+import { useTheme } from '@pancakeswap/hooks'
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
 import { useAllowRecipient } from '../../Swap/V3Swap/hooks'
+
+import ArrowDark from '../../../../public/images/swap/arrow_dark.json' assert { type: 'json' }
+import ArrowLight from '../../../../public/images/swap/arrow_light.json' assert { type: 'json' }
 
 export const Line = styled.div`
   position: absolute;
@@ -23,7 +27,10 @@ export const Line = styled.div`
 `
 
 export const FlipButton = memo(function FlipButton() {
-  // const lottieRef = useRef<LottieRefCurrentProps | null>(null)
+  const lottieRef = useRef<LottieRefCurrentProps | null>(null)
+  const { isDark } = useTheme()
+
+  const animationData = useMemo(() => (isDark ? ArrowDark : ArrowLight), [isDark])
 
   const { onSwitchTokens } = useSwapActionHandlers()
   const {
@@ -41,18 +48,18 @@ export const FlipButton = memo(function FlipButton() {
     <AutoColumn justify="space-between" position="relative">
       <Line />
       <AutoRow justify="center" style={{ padding: '0 1rem', marginTop: '1em' }}>
-        <SwapUIV2.SwitchButtonV2 onClick={onFlip} />
+        {/* <SwapUIV2.SwitchButtonV2 onClick={onFlip} /> */}
 
-        {/* <Lottie
+        <Lottie
           lottieRef={lottieRef}
-          animationData={ArrowLottie}
-          style={{ height: '78px', cursor: 'pointer' }}
+          animationData={animationData}
+          style={{ height: '48px', cursor: 'pointer' }}
           onClick={onFlip}
           autoplay={false}
           loop={false}
-          onMouseEnter={() => lottieRef.current?.playSegments([19, 32], true)}
-          onMouseLeave={() => lottieRef.current?.playSegments([52, 73], true)}
-        /> */}
+          onMouseEnter={() => lottieRef.current?.playSegments(isDark ? [7, 23] : [19, 32], true)}
+          onMouseLeave={() => lottieRef.current?.playSegments(isDark ? [39, 62] : [52, 73], true)}
+        />
       </AutoRow>
     </AutoColumn>
   )
